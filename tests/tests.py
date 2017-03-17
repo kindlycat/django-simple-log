8477e5b3620dfc7bb20e9437aa94d16882813cf5
kindlycat <grishkokot@gmail.com>
2017-03-17 19:36:49 +0300

    Add tests for custom views

--- tests/tests.py
+++ tests/tests.py
@@ -48,6 +48,7 @@ def disconnect_signals(sender=None):
 
 class AdminTestCase(TestCase):
     model = TestModel
+    namespace = 'admin:'
 
     def setUp(self):
         self.user = User.objects.create_superuser('user', 'test@example.com',
@@ -55,29 +56,29 @@ class AdminTestCase(TestCase):
         self.other_model = OtherModel.objects.create(char_field='other')
         self.client.login(username='user', password='pass')
 
-    @classmethod
-    def tearDown(cls):
-        SimpleLog.objects.all().delete()
-
     def get_add_url(self, model):
         return reverse(
-            'admin:{}_{}_add'.format(model._meta.app_label,
-                                     model._meta.model_name)
+            '{}{}_{}_add'.format(self.namespace, model._meta.app_label,
+                                 model._meta.model_name)
         )
 
     def get_change_url(self, model, *args, **kwargs):
         return reverse(
-            'admin:{}_{}_change'.format(model._meta.app_label,
-                                        model._meta.model_name),
+            '{}{}_{}_change'.format(self.namespace, model._meta.app_label,
+                                    model._meta.model_name),
             args=args, kwargs=kwargs
         )
 
     def get_delete_url(self, model, *args, **kwargs):
         return reverse(
-            'admin:{}_{}_delete'.format(model._meta.app_label,
-                                        model._meta.model_name),
+            '{}{}_{}_delete'.format(self.namespace, model._meta.app_label,
+                                    model._meta.model_name),
             args=args, kwargs=kwargs
         )
+
+    @classmethod
+    def tearDown(cls):
+        SimpleLog.objects.all().delete()
 
     def test_add_object_all_field_filled(self):
         initial_count = SimpleLog.objects.count()
@@ -471,6 +472,219 @@ class AdminTestCase(TestCase):
         sl = SimpleLog.objects.first()
         self.assertEqual(SimpleLog.objects.count(), initial_count + 3)
         self.assertIsNone(sl.user_ip)
+
+
+class CustomViewTestCase(AdminTestCase):
+    namespace = ''
+
+    def test_anonymous_add(self):
+        self.client.logout()
+        initial_count = SimpleLog.objects.count()
+        params = {
+            'char_field': 'test',
+            'fk_field': self.other_model.pk,
+            'm2m_field': [self.other_model.pk],
+            'choice_field': TestModel.TWO
+        }
+        self.client.post(self.get_add_url(self.model), data=params)
+        new_obj = TestModel.objects.last()
+        self.assertEqual(SimpleLog.objects.count(), initial_count + 1)
+        sl = SimpleLog.objects.first()
+        self.assertEqual(sl.action_flag, SimpleLog.ADD)
+        self.assertIsNone(sl.user)
+        self.assertEqual(sl.user_repr, settings.ANONYMOUS_REPR)
+        self.assertEqual(sl.user_ip, '127.0.0.1')
+        self.assertEqual(sl.object_id, force_text(new_obj.id))
+        self.assertEqual(sl.object_repr, force_text(new_obj))
+        self.assertEqual(sl.content_type,
+                         ContentType.objects.get_for_model(new_obj))
+        self.assertIsNone(sl.old)
+        self.assertDictEqual(
+            sl.new,
+            {
+                'char_field': {
+                    'label': 'Char field',
+                    'value': 'test'
+                },
+                'fk_field': {
+                    'label': 'Fk field',
+                    'value': {
+                        'db': force_text(self.other_model.pk),
+                        'repr': force_text(self.other_model),
+                    }
+                },
+                'm2m_field': {
+                    'label': 'M2m field',
+                    'value': [{
+                        'db': force_text(self.other_model.pk),
+                        'repr': force_text(self.other_model),
+                    }]
+                },
+                'choice_field': {
+                    'label': 'Choice field',
+                    'value': {
+                        'db': force_text(TestModel.TWO),
+                        'repr': 'Two'
+                    }
+                }
+            }
+        )
+
+    def test_anonymous_change(self):
+        self.client.logout()
+        params = {
+            'char_field': 'test',
+            'fk_field': self.other_model.pk,
+            'm2m_field': [self.other_model.pk],
+            'choice_field': TestModel.ONE
+        }
+        self.client.post(self.get_add_url(self.model), data=params)
+        obj = TestModel.objects.last()
+        initial_count = SimpleLog.objects.count()
+        params = {
+            'char_field': 'test2',
+            'fk_field': '',
+            'm2m_field': [],
+            'choice_field': TestModel.TWO
+        }
+        self.client.post(self.get_change_url(self.model, obj.pk), data=params)
+        self.assertEqual(SimpleLog.objects.count(), initial_count + 1)
+        sl = SimpleLog.objects.first()
+        obj.refresh_from_db()
+        self.assertEqual(sl.action_flag, SimpleLog.CHANGE)
+        self.assertIsNone(sl.user)
+        self.assertEqual(sl.user_repr, settings.ANONYMOUS_REPR)
+        self.assertEqual(sl.user_ip, '127.0.0.1')
+        self.assertEqual(sl.object_id, force_text(obj.id))
+        self.assertEqual(sl.object_repr, force_text(obj))
+        self.assertEqual(sl.content_type,
+                         ContentType.objects.get_for_model(obj))
+        self.assertDictEqual(
+            sl.old,
+            {
+                'char_field': {
+                    'label': 'Char field',
+                    'value': 'test'
+                },
+                'fk_field': {
+                    'label': 'Fk field',
+                    'value': {
+                        'db': force_text(self.other_model.pk),
+                        'repr': force_text(self.other_model),
+                    }
+                },
+                'm2m_field': {
+                    'label': 'M2m field',
+                    'value': [{
+                        'db': force_text(self.other_model.pk),
+                        'repr': force_text(self.other_model),
+                    }]
+                },
+                'choice_field': {
+                    'label': 'Choice field',
+                    'value': {
+                        'db': force_text(TestModel.ONE),
+                        'repr': 'One'
+                    }
+                }
+            }
+        )
+        self.assertDictEqual(
+            sl.new,
+            {
+                'char_field': {
+                    'label': 'Char field',
+                    'value': 'test2'
+                },
+                'fk_field': {
+                    'label': 'Fk field',
+                    'value': {
+                        'db': None,
+                        'repr': '',
+                    }
+                },
+                'm2m_field': {
+                    'label': 'M2m field',
+                    'value': []
+                },
+                'choice_field': {
+                    'label': 'Choice field',
+                    'value': {
+                        'db': force_text(TestModel.TWO),
+                        'repr': 'Two'
+                    }
+                }
+            }
+        )
+
+    def test_delete_object_check_log(self):
+        self.client.logout()
+        params = {
+            'char_field': 'test',
+            'fk_field': self.other_model.pk,
+            'm2m_field': [self.other_model.pk],
+            'choice_field': TestModel.TWO
+        }
+        self.client.post(self.get_add_url(self.model), data=params)
+        obj = TestModel.objects.last()
+        initial_count = SimpleLog.objects.count()
+        self.client.post(self.get_delete_url(self.model, obj.pk),
+                         data={'post': 'yes'})
+        self.assertEqual(SimpleLog.objects.count(), initial_count + 1)
+        sl = SimpleLog.objects.first()
+        self.assertEqual(sl.action_flag, SimpleLog.DELETE)
+        self.assertIsNone(sl.user)
+        self.assertEqual(sl.user_repr, settings.ANONYMOUS_REPR)
+        self.assertEqual(sl.user_ip, '127.0.0.1')
+        self.assertEqual(sl.object_id, force_text(obj.id))
+        self.assertEqual(sl.object_repr, force_text(obj))
+        self.assertEqual(sl.content_type,
+                         ContentType.objects.get_for_model(obj))
+        self.assertIsNone(sl.new)
+        self.assertDictEqual(
+            sl.old,
+            {
+                'char_field': {
+                    'label': 'Char field',
+                    'value': 'test'
+                },
+                'fk_field': {
+                    'label': 'Fk field',
+                    'value': {
+                        'db': force_text(self.other_model.pk),
+                        'repr': force_text(self.other_model),
+                    }
+                },
+                'm2m_field': {
+                    'label': 'M2m field',
+                    'value': [{
+                        'db': force_text(self.other_model.pk),
+                        'repr': force_text(self.other_model),
+                    }]
+                },
+                'choice_field': {
+                    'label': 'Choice field',
+                    'value': {
+                        'db': force_text(TestModel.TWO),
+                        'repr': 'Two'
+                    }
+                }
+            }
+        )
+
+    @override_settings(SIMPLE_LOG_ANONYMOUS_REPR='UNKNOWN')
+    def test_anonymous_change_repr(self):
+        self.client.logout()
+        params = {
+            'char_field': 'test',
+            'fk_field': self.other_model.pk,
+            'm2m_field': [self.other_model.pk],
+            'choice_field': TestModel.TWO
+        }
+        self.client.post(self.get_add_url(self.model), data=params)
+        sl = SimpleLog.objects.first()
+        self.assertIsNone(sl.user)
+        self.assertEqual(sl.user_repr, 'UNKNOWN')
 
 
 class SettingsTestCase(TestCase):
--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,72 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import UpdateView
+
+from tests.test_app.models import TestModel, OtherModel
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(
+        r'^test_model/add/$',
+        CreateView.as_view(
+            model=TestModel,
+            fields='__all__',
+            success_url='.',
+            template_name='form.html'
+        ),
+        name='test_app_testmodel_add'
+    ),
+    url(
+        r'^test_model/(?P<pk>\d+)/$',
+        UpdateView.as_view(
+            model=TestModel,
+            fields='__all__',
+            success_url='.',
+            template_name='form.html'
+        ),
+        name='test_app_testmodel_change'
+    ),
+    url(
+        r'^test_model/(?P<pk>\d+)/delete/$',
+        DeleteView.as_view(
+            model=TestModel,
+            success_url='.'
+        ),
+        name='test_app_testmodel_delete'
+    ),
+
+    url(
+        r'^other_model/add/$',
+        CreateView.as_view(
+            model=OtherModel,
+            fields='__all__',
+            success_url='.',
+            template_name='form.html'
+        ),
+        name='test_app_othermodel_add'
+    ),
+    url(
+        r'^other_model/(?P<pk>\d+)/$',
+        UpdateView.as_view(
+            model=OtherModel,
+            fields='__all__',
+            success_url='.',
+            template_name='form.html'
+        ),
+        name='test_app_othermodel_change'
+    ),
+    url(
+        r'^other_model/(?P<pk>\d+)/delete/$',
+        DeleteView.as_view(
+            model=OtherModel,
+            success_url='.'
+        ),
+        name='test_app_othermodel_delete'
+    ),
 ]
