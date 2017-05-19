10b9c5513eacd9ff8e9d5072628fd9477d4e0fdf
kindlycat <grishkokot@gmail.com>
2017-05-20 02:44:12 +0300

    fix build

--- tests/test_app/migrations/0001_initial.py
+++ tests/test_app/migrations/0001_initial.py
@@ -3,7 +3,6 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-import django.contrib.postgres.fields.jsonb
 from django.db import migrations, models
 import django.db.models.deletion
 import django.utils.timezone
