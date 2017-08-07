3254dccb4edbc35c9845e99420cb1ed6810d19a3
kindlycat <grishkokot@gmail.com>
2017-08-07 17:56:23 +0300

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name='django-simple-log',
-    version='0.1.8',
+    version='0.1.9',
     description='Logging django models changes.',
     long_description=readme(),
     keywords='django log audit history',
