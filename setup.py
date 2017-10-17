10d272ec830899a969cd5dbbd3c756c34ca8bee9
kindlycat <grishkokot@gmail.com>
2017-10-17 03:27:51 +0300

    update admin

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name='django-simple-log',
-    version='0.1.16',
+    version='0.1.17',
     description='Logging django models changes.',
     long_description=readme(),
     keywords=['django', 'log', 'audit', 'history'],
