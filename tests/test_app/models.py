be6ae9853708f4c375131b779a180425e055ce3d
kindlycat <grishkokot@gmail.com>
2019-04-08 14:09:09 +0300

    Fixes for django master

--- tests/test_app/models.py
+++ tests/test_app/models.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from six import python_2_unicode_compatible
+
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 from simple_log.models import ModelSerializer, SimpleLogAbstract
 
