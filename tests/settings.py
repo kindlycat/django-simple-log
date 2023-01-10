8a4f1455c38a7eb97a84defb417be2d49cf3e1d0
kindlycat <grishkokot@gmail.com>
2023-01-10 18:49:41 +0300

    Github actions

--- tests/settings.py
+++ tests/settings.py
@@ -55,8 +55,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'simple_log'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'host': os.environ.get('POSTGRES_HOST', 'localhost'),
-        'port': os.environ.get('POSTGRES_PORT', 5432),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
     }
 }
 
